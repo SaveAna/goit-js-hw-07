2c87d43f81237730edad7243054d64f1d86e8696
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-18 20:59:23 +0200

    add elements of gallery (01-gallery)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const makeImgElem = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </li>`;
+
+const galleryElements = galleryItems.map(makeImgElem).join('');
+gallery.insertAdjacentHTML('afterbegin', galleryElements);
 
 console.log(galleryItems);

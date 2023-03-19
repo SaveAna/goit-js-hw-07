1681c1038a37ceb0c91472c33dc40140a8ad8437
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-19 12:42:04 +0200

    add lightbox (02-lightbox ready)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,26 +12,6 @@ const makeImgElem = ({ preview, original, description }) =>
 
 const galleryElements = galleryItems.map(makeImgElem).join('');
 gallery.insertAdjacentHTML('afterbegin', galleryElements);
-
-gallery.addEventListener('click', onImageClick);
-
-function onImageClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  //   const imgDataSource = event.target.getAttribute('data-source');
-
-  //   const instance = basicLightbox.create(`<img src = "${imgDataSource}">`);
-  //   instance.show(() => document.addEventListener('keydown', onDocumentKeyDown));
-
-  //   function onDocumentKeyDown(event) {
-  //     if (event.code !== 'Escape') {
-  //       return;
-  //     }
-  //     instance.close(() => document.removeEventListener('keydown', onDocumentKeyDown));
-  //   }
-}
+new SimpleLightbox('.gallery .gallery__link', { captionsData: 'alt', captionDelay: 250 });
 
 console.log(galleryItems);
